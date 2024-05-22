019f94c63af7d2ccc6afc02fa9d310a787b737ad
nicolas.nos <nicolas.nos@hotmail.com>
2024-05-22 18:03:42 -0500

    style: footer corrections

--- src/Molecules/FooterV2.jsx
+++ src/Molecules/FooterV2.jsx
@@ -59,8 +59,8 @@ const FooterV2 = () => {
             <figure className="kiosco">
               <img src={kiosco} alt="Kiosco" />
               <svg
-                width="35"
-                height="35"
+                width="45"
+                height="45"
                 viewBox="0 0 35 35"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
